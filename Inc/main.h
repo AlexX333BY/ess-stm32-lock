d4350d17c5312a35caf78989e3c97aea1d4c0e3a
Aliaksei <alexx333by@protonmail.com>
2019-11-16 22:30:59 +0300

    Project base generated

---
+++ Inc/main.h
@@ -0,0 +1,116 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define DISPLAY_G_Pin GPIO_PIN_0
+#define DISPLAY_G_GPIO_Port GPIOA
+#define DISPLAY_F_Pin GPIO_PIN_1
+#define DISPLAY_F_GPIO_Port GPIOA
+#define DISPLAY_E_Pin GPIO_PIN_2
+#define DISPLAY_E_GPIO_Port GPIOA
+#define DISPLAY_D_Pin GPIO_PIN_3
+#define DISPLAY_D_GPIO_Port GPIOA
+#define DISPLAY_C_Pin GPIO_PIN_4
+#define DISPLAY_C_GPIO_Port GPIOA
+#define DISPLAY_B_Pin GPIO_PIN_5
+#define DISPLAY_B_GPIO_Port GPIOA
+#define DISPLAY_A_Pin GPIO_PIN_6
+#define DISPLAY_A_GPIO_Port GPIOA
+#define KEYPAD_A_Pin GPIO_PIN_0
+#define KEYPAD_A_GPIO_Port GPIOB
+#define KEYPAD_B_Pin GPIO_PIN_1
+#define KEYPAD_B_GPIO_Port GPIOB
+#define KEYPAD_C_Pin GPIO_PIN_2
+#define KEYPAD_C_GPIO_Port GPIOB
+#define DISPLAY_1_Pin GPIO_PIN_8
+#define DISPLAY_1_GPIO_Port GPIOA
+#define DISPLAY_2_Pin GPIO_PIN_9
+#define DISPLAY_2_GPIO_Port GPIOA
+#define DISPLAY_3_Pin GPIO_PIN_10
+#define DISPLAY_3_GPIO_Port GPIOA
+#define DISPLAY_4_Pin GPIO_PIN_11
+#define DISPLAY_4_GPIO_Port GPIOA
+#define LED_RED_Pin GPIO_PIN_13
+#define LED_RED_GPIO_Port GPIOA
+#define LED_YELLOW_Pin GPIO_PIN_14
+#define LED_YELLOW_GPIO_Port GPIOA
+#define LED_GREEN_Pin GPIO_PIN_15
+#define LED_GREEN_GPIO_Port GPIOA
+#define KEYPAD_D_Pin GPIO_PIN_3
+#define KEYPAD_D_GPIO_Port GPIOB
+#define KEYPAD_1_Pin GPIO_PIN_5
+#define KEYPAD_1_GPIO_Port GPIOB
+#define KEYPAD_1_EXTI_IRQn EXTI9_5_IRQn
+#define KEYPAD_2_Pin GPIO_PIN_6
+#define KEYPAD_2_GPIO_Port GPIOB
+#define KEYPAD_2_EXTI_IRQn EXTI9_5_IRQn
+#define KEYPAD_3_Pin GPIO_PIN_7
+#define KEYPAD_3_GPIO_Port GPIOB
+#define KEYPAD_3_EXTI_IRQn EXTI9_5_IRQn
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
